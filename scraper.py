15f272ef71c10600b7a52ad683c892fcd72918e9
d541 <prince4albert@gmail.com>
2025-09-10 14:43:26 +0100

    ...

--- scraper.py
+++ scraper.py
@@ -14,3 +14,16 @@ import pandas as pd
 from datetime import datetime, timedelta
 from typing import List, Optional, Dict
 from dataclasses import dataclass, asdict
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import (
+    TimeoutException, 
+    NoSuchElementException, 
+    WebDriverException,
+    ElementNotInteractableException
+)
